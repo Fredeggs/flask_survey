7b4973706e812d76c35569c1b6ac6118f0548758
Fredeggs <ian.dauphin@gmail.com>
2022-05-22 23:24:39 -0400

    added sessions functionality

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,6 +1,6 @@
 from crypt import methods
 from http.client import responses
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -8,7 +8,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'shushhhhh'
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
-responses = []
 question_num = 0
 
 @app.route('/')
@@ -16,13 +15,19 @@ def show_home_page():
     return render_template('home.html')
 
 
+@app.route('/initialize-responses', methods=['POST'])
+def create_response_session():
+    if len(session.get('responses', [])) == len(satisfaction_survey.questions):
+        flash("You have already completed the survey, doofus!")
+        return redirect('/thankyou')
+    session['responses'] = []
+    return redirect('/questions/0')
+
+
 @app.route('/questions/<num>')
 def get_question_page(num):
     global question_num
     q_num = int(num)
-    if len(responses) == len(satisfaction_survey.questions):
-        flash("You have already completed the survey, doofus!")
-        return redirect('/thankyou')
     if q_num == question_num:
         question = satisfaction_survey.questions[q_num].question
         return render_template('question.html', question=question)
@@ -36,8 +41,10 @@ def show_thankyou():
 
 @app.route("/answer", methods=["POST"])
 def process_answer():
+    responses = session['responses']
     answer = request.form["answer"]
     responses.append(answer)
+    session['responses'] = responses
     global question_num
     question_num += 1
     if question_num >= len(satisfaction_survey.questions):
